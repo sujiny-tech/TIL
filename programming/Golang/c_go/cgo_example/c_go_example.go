cb6808d1131d769f0df1d3063e75c1fb6ed12df4
sujiny-tech <72974863+sujiny-tech@users.noreply.github.com>
2022-02-18 10:01:40 +0900

    Add files via upload

--- programming/Golang/c_go/cgo_example/c_go_example.go
+++ programming/Golang/c_go/cgo_example/c_go_example.go
@@ -7,15 +7,14 @@ int sum_fun(int a, int b) {
 	return a+b;
 }
 void print_hello() {
-	printf("Hello, world! \n");
+	printf("Hello world~~~~ \n");
 }*/
 import "C"
 import "fmt"
 
 func main() {
-	var a, b int = 1, 2
 	C.print_hello()
-	r := C.sum_fun(C.int(a), C.int(b))
+	r := C.sum_fun(C.int(2), C.int(22))
 	fmt.Println(r)
 
 }
--- programming/Golang/c_go/cgo_example/c_rand_example.go
+++ programming/Golang/c_go/cgo_example/c_rand_example.go
@@ -7,8 +7,8 @@ import "C"
 import "fmt"
 
 func main() {
-	r := C.rand()
-	//c의 rand함수 사용하려면 주석안에 해당 헤더파일 포함 필요 (line 3~5)
-	//위의 주석과 C를 import하는 해당부분이 이렇게 붙어있어야 함!! (line 3~6)
-	fmt.Println(r)
+	res := C.rand()
+	//c의 rand함수 사용하려면 주석안에 해당 헤더파일 포함 필요
+	//위의 주석과 C를 import하는 해당부분이 이렇게 붙어있어야 함!!
+	fmt.Println(res)
 }
